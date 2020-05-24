0ad3e74c34136a5bf4a85113f20cbb78040a1b63
Ronit Samaddar <ronitsamaddar97@gmail.com>
2020-05-24 17:54:02 +0530

    Single Linked List Implementation :         Finished deletion

--- DataStructures/LinkedList/Basic Implementation/SinglyLinkedList.cpp
+++ DataStructures/LinkedList/Basic Implementation/SinglyLinkedList.cpp
@@ -49,7 +49,8 @@ public:
 	void insert_start(T val)
 	{
 		//Inserts node with value val at the beginning of the list
-		//Complexity = O(1)
+		//Time Complexity = O(1)
+		//Space Complexity = O(1)
 		Node<T> *temp=new Node<T>(val);
 		if(this->start==NULL)
 		{
@@ -66,7 +67,8 @@ public:
 	void insert_end(T val)
 	{
 		//Inserts node with value val at the end of the list
-		//Complexity = O(n)
+		//Time Complexity = O(n)
+		//Space Complexity = O(1)
 
 		Node<T> *temp=new Node<T>(val);
 		if(this->start==NULL)
@@ -88,7 +90,8 @@ public:
 	void insert_pos(T val,int pos)
 	{
 		//Inserts node with value val at the position pos
-		//Complexity = O(n)
+		//Time Complexity = O(n)
+		//Space Complexity = O(1)
 
 		if(pos==1)
 		{
@@ -123,6 +126,122 @@ public:
 			}
 		}
 	}
+	void delete_start()
+	{
+		//Deletes the node at the beginning of the list
+		//Time Complexity = O(1)
+		//Space Complexity = O(1)
+
+		if(this->start==NULL)
+		{
+			//Empty List Case
+			cout<<"List is Empty!!!"<<endl;
+		}
+		else
+		{
+			//Storing the first node and taking start pointer to the second node
+			Node<T> *temp=this->start;
+			this->start=this->start->get_next();
+
+			//Isolating the first node and freeing its allocated memory
+			temp->set_next(NULL);
+			delete(temp);
+		}
+	}
+	void delete_end()
+	{
+		//Deletes the node at the end of the list
+		//Time Complexity = O(n)
+		//Space Complexity = O(1)
+
+		if(this->start==NULL)
+		{
+			//Empty List Case
+			cout<<"List is Empty!!!"<<endl;
+		}
+		else if(this->start->get_next()==NULL)
+		{
+			//Only one element
+			//This will be same as deleting the first element
+			this->delete_start();
+
+		}
+		else
+		{
+			//Traversing to the node before the end node
+			Node<T> *temp=this->start;
+			while((temp->get_next())->get_next()!=NULL)
+			{
+				temp=temp->get_next();
+			}
+			//Loop will stop with temp being the penultimate node
+			Node<T> *p=temp->get_next();
+			temp->set_next(NULL);
+			delete(p);
+		}
+
+	}
+	void delete_pos(int pos)
+	{
+		//Deletes the node at the position pos
+		//Time Complexity = O(n)
+		//Space Complexity = O(1)
+
+		if(this->start==NULL)
+		{
+			//Empty List Case
+			cout<<"List is Empty!!!"<<endl;
+		}
+		else if(this->start->get_next()==NULL)
+		{
+			//Single element list case
+			if(pos==1)
+			{
+				//Same as delete beginning
+				this->delete_start();
+			}
+			else
+			{
+				cout<<"INVALID POSITION!! NO DELETION DONE."<<endl;
+			}
+		}
+		else
+		{
+			if(pos==1)
+			{
+				//Same as delete beginning
+				this->delete_start();
+			}
+			else
+			{
+				//General case (List contains more than one node and pos >1)
+				int index=1;							//Current node number
+				Node<T> *p=this->start;
+				while(1)
+				{
+					if((p->get_next()==NULL)||(index==pos-1))		//We must stop at (pos-1)-th node
+					{
+						break;
+					}
+					p=p->get_next();
+					index++;
+				}
+				if(p->get_next()==NULL && index<pos-1)
+				{
+					//If we pos>(number of elements), then pos is invalid
+					cout<<"INVALID POSITION!! NO DELETION DONE."<<endl;
+				}
+				else
+				{
+					//We have stopped at the previous node
+					Node<T> *temp=p->get_next();
+					p->set_next(temp->get_next());
+					delete(temp);
+				}
+			}
+		}
+		
+	}
 	void print_list()
 	{
 		//Prints each node as we traverse the entire list
@@ -148,5 +267,9 @@ int main()
 	L->insert_pos(4,2);
 	L->insert_pos(5,4);
 	L->insert_pos(6,6);
+	L->print_list();
+	L->delete_start();
+	L->delete_end();
+	L->delete_pos(3);
 	L->print_list();
 }
