3273458a79df6b541cd594de6e93bf1edaaeed00
Ronit Samaddar <ronitsamaddar97@gmail.com>
2020-05-24 17:24:44 +0530

    Single Linked List Implementation :         Finished insertion at end and at random position p

--- DataStructures/LinkedList/Basic Implementation/SinglyLinkedList.cpp
+++ DataStructures/LinkedList/Basic Implementation/SinglyLinkedList.cpp
@@ -48,6 +48,8 @@ public:
 	}
 	void insert_start(T val)
 	{
+		//Inserts node with value val at the beginning of the list
+		//Complexity = O(1)
 		Node<T> *temp=new Node<T>(val);
 		if(this->start==NULL)
 		{
@@ -61,8 +63,71 @@ public:
 			this->start=temp;
 		}
 	}
+	void insert_end(T val)
+	{
+		//Inserts node with value val at the end of the list
+		//Complexity = O(n)
+
+		Node<T> *temp=new Node<T>(val);
+		if(this->start==NULL)
+		{
+			//Empty List Case
+			this->start=temp;
+		}
+		else
+		{
+			//General case
+			Node<int> *p=this->start;
+			while(p->get_next()!=NULL)
+			{
+				p=p->get_next();
+			}
+			p->set_next(temp);
+		}
+	}
+	void insert_pos(T val,int pos)
+	{
+		//Inserts node with value val at the position pos
+		//Complexity = O(n)
+
+		if(pos==1)
+		{
+			//Insert at beginning case
+			this->insert_start(val);
+		}
+		else
+		{
+			//General case
+			int index=1;							//Current node number
+			Node<T> *temp=new Node<T>(val);
+			Node<T> *p=this->start;
+			while(1)
+			{
+				if((p==NULL)||(index==pos-1))		//We must stop at (pos-1)-th node
+				{
+					break;
+				}
+				p=p->get_next();
+				index++;
+			}
+			if(p==NULL && index<pos-1)
+			{
+				//If we pos>(number of elements+1), then pos is invalid
+				cout<<"INVALID POSITION!! NO INSERTION DONE."<<endl;
+			}
+			else
+			{
+				//We have stopped at the previous node
+				temp->set_next(p->get_next());
+				p->set_next(temp);
+			}
+		}
+	}
 	void print_list()
 	{
+		//Prints each node as we traverse the entire list
+		//Complexity = O(n)
+
 		Node<T> *temp=start;
 		while(temp!=NULL)
 		{
@@ -77,7 +142,11 @@ public:
 int main()
 {
 	LinkedList<int> *L=new LinkedList<int>();
-	L->insert_start(1);
+	L->insert_end(1);
 	L->insert_start(2);
+	L->insert_end(3);
+	L->insert_pos(4,2);
+	L->insert_pos(5,4);
+	L->insert_pos(6,6);
 	L->print_list();
 }
